8ab8a459bd195ab7b2f41d602fcf65a8c1f42f2e
Benedikt Kusemann <b.kusemann@sofort.com>
2017-04-08 12:52:07 +0200

    CleanUp

---
+++ src/apps/main/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+const rootElement = (
+  <h1>ICE, ICE, Baby...</h1>
+)
+
+export default function (root) {
+  render(rootElement, root)
+}
--- src/index.js
+++ src/index.js
@@ -1,31 +1,3 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import createStore from './store/createStore'
-import AppContainer from './containers/AppContainer'
+import renderApplication from './apps/main'
 
-// import * as Button from '@klarna/ui/Button'
-
-// ========================================================
-// Store Instantiation
-// ========================================================
-const initialState = window.__INITIAL_STATE__
-const store = createStore(initialState)
-
-// ========================================================
-// Render Setup
-// ========================================================
-const MOUNT_NODE = document.getElementById('root')
-
-let render = () => {
-  const routes = require('./routes/index').default(store)
-
-  ReactDOM.render(
-    <AppContainer store={store} routes={routes} />,
-    MOUNT_NODE
-  )
-}
-
-// ========================================================
-// Go!
-// ========================================================
-render()
+renderApplication(document.getElementById('root'))
